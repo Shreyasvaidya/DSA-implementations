fbb66fddb147dc027c79db53fd8ed37b2cf6626d
Shreyas Vaidya <95679215+Shreyasvaidya@users.noreply.github.com>
2022-08-10 22:29:23 +0530

    Create stack.h

---
+++ stack.h
@@ -0,0 +1,47 @@
+class Stack{
+	private:
+		int array[200000];//Random large number, INT_MAX giving segfault
+		int head = -1;
+		
+	public:
+		bool empty(){
+			if (head ==-1){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+    void push(int elem){
+			head++;
+			array[head] = elem;
+		}
+		
+		void pop(){
+			if (this -> empty()){
+				cout<<"stack is empty"<<endl;
+			}
+			else{
+				head --;
+			}
+		}
+		int top(){
+			if (this ->empty()){
+				return -1;
+			}
+			else{
+				return array[head];
+			}
+		}
+		void size(){
+			cout<<head+1<<endl;
+		}
+		void isempty(){
+			if (this -> empty()){
+				cout << 1<<endl;
+			}
+			else{
+				cout <<0<<endl;
+			}
+		}
+};
