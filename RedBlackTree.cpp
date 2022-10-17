9811dc2dae5570bc9dfa804b1d5faf6c955bc7af
Shreyas Vaidya <95679215+Shreyasvaidya@users.noreply.github.com>
2022-10-17 19:00:40 +0530

    removed extra code

--- RedBlackTree.cpp
+++ RedBlackTree.cpp
@@ -1,47 +1,6 @@
 #include<iostream>
 using namespace std;
-template<class T>
-class Stack{
-	private:
-		T array[200000];//Random large number, INT_MAX giving segfault
-		int head = -1;
-		bool empty(){
-			if (head ==-1){
-				return true;
-			}
-			else{
-				return false;
-			}
-		}
-	public:
-		void Push(T elem){
-			head++;
-			array[head] = elem;
-		}
-		
-		void Pop(){
-			if ( empty()){
-				cout<<"stack is empty"<<endl;
-			}
-			else{
-				head --;
-			}
-		}
-		T peek(){
-			return array[head];
-		}
-		void size(){
-			cout<<head+1<<endl;
-		}
-		void isempty(){
-			if (this -> empty()){
-				cout << 1<<endl;
-			}
-			else{
-				cout <<0<<endl;
-			}
-		}
-};
+
 class node{
     public:
     int data;
@@ -194,7 +153,6 @@ class RedBlackTree{
 
 
 int main(){
-        #test
         RedBlackTree* tree = new RedBlackTree(5);
         
 
